148f290c4b864cafdb98a09f93269f196630fc32
peteandmad <campcame12@gmail.com>
2023-02-06 12:41:50 +0700

    Update orderRouter.js

--- src/routes/api/v1/orderRouter.js
+++ src/routes/api/v1/orderRouter.js
@@ -1,18 +1,25 @@
-const router = require('express').Router();
-const orderController = require('../../../controllers/v1/Order.controller');
-const auth = require('../../../utils/auth');
-
+const router = require("express").Router();
+const orderController = require("../../../controllers/v1/Order.Controller");
+const auth = require("../../../utils/auth");
 
 // Order templates //
 /**
  * /api/v1/order/create-order-temp
  * */
-router.post('/create-order-temp',auth.isAuth, orderController.createOrderTemplate);
+router.post(
+  "/create-order-temp",
+  auth.isAuth,
+  orderController.createOrderTemplate
+);
 
 /**
  * /api/v1/order/update-order-temp/:orderTempId
  * */
-router.put('/update-order-temp/:orderTempId',auth.isAuth, orderController.updateOrderTemplate);
+router.put(
+  "/update-order-temp/:orderTempId",
+  auth.isAuth,
+  orderController.updateOrderTemplate
+);
 
 /**
  * /api/v1/order/order-temp-list
@@ -22,18 +29,27 @@ router.get("/order-temp-list", orderController.getOrderTempList);
 /**
  * /api/v1/order/create-order-temp-prod
  * */
-router.post("/create-order-temp-prod", orderController.createOrderTemplateProduct);
+router.post(
+  "/create-order-temp-prod",
+  orderController.createOrderTemplateProduct
+);
 
 /**
  * /api/v1/order/create-order-temp-prod-list
  * */
-router.post("/create-order-temp-prod-list", orderController.createOrderProductTempList);
+router.post(
+  "/create-order-temp-prod-list",
+  orderController.createOrderProductTempList
+);
 
 /**
  * /api/v1/order/delete-product-temp/{id} DELETE
  */
-router.delete('/delete-product-temp/:id',auth.isAuth,orderController.deleteProdTemp);
-
+router.delete(
+  "/delete-product-temp/:id",
+  auth.isAuth,
+  orderController.deleteProdTemp
+);
 
 // Order templates //
 
